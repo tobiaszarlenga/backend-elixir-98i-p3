cea99913f2f50cba9a0faf48417c9a846341660b
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-08-14 16:20:05 -0300

    agrego put de blogs

--- src/controllers/blogs/index.js
+++ src/controllers/blogs/index.js
@@ -1,9 +1,11 @@
 import { GetController } from './controllers/GetController.js';
 import { PostController } from './controllers/PostController.js';
+import { PutController } from './controllers/PutController.js';
 
-export const blogs = {
+export const Blogs = {
   GetController,
   PostController,
+  PutController,
   // put controller,
   // delete controller,
 };
