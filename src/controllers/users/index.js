570de2ac2b6ec8bff20984019a8598b83595d5b2
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-08-20 12:43:27 -0300

    agrego recursos de user, y validacion body de usuarios

---
+++ src/controllers/users/index.js
@@ -0,0 +1,11 @@
+// import { DeleteController } from './controllers/DeleteController.js';
+import { GetController } from './controllers/GetController.js';
+import { PostController } from './controllers/PostController.js';
+// import { PutController } from './controllers/PutController.js';
+
+export const Users = {
+  GetController,
+  PostController,
+  //   PutController,
+  //   DeleteController,
+};
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
 import { blogRouter } from './routers/blogRouter.js';
+import { userRouter } from './routers/userRouter.js';
 
 export const mainRouter = express.Router();
 
 mainRouter.use('/blogs', blogRouter);
+mainRouter.use('/users', userRouter);
