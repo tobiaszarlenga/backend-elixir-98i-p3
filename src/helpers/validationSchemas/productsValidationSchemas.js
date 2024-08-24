01e4dbe7d93c1ceb86b3188ed67e824490449926
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-08-23 21:38:44 -0300

    creo los inputs (productShema) que va a recibir el front y tambien agrego las validaciones para cada producto

--- src/helpers/validationSchemas/productsValidationSchemas.js
+++ src/helpers/validationSchemas/productsValidationSchemas.js
@@ -1,21 +1,46 @@
 import joi from 'joi';
 
 export const post_put_productValidationSchema = joi.object({
-  title: joi.string().trim().min(3).max(30).required().messages({
-    'string.min': 'El título debe tener al menos 3 caracteres',
-    'string.max': 'El título debe tener menos de 30 caracteres',
-    'any.required': 'El título es obligatorio',
-    '*': "revisa el campo 'title' ",
+  nameP: joi.string().trim().min(3).max(30).required().messages({
+    'string.min': 'El nombre debe tener al menos 3 caracteres',
+    'string.max': 'El nombre debe tener menos de 30 caracteres',
+    'any.required': 'El nombre es obligatorio',
+    '*': "revisa el campo 'nameP' ",
   }),
-  imageUrl: joi.string().trim().min(3).uri().required().messages({
-    'string.uri': "El campo 'imageUrl' debe tener una url valida",
-    'any.required': "El campo 'imageUrl' es obligatorio",
-    '*': "revisa el campo 'imageUrl' ",
+  img: joi.string().trim().min(3).uri().required().messages({
+    'string.uri': "El campo 'img' debe tener una URL válida",
+    'any.required': "El campo 'img' es obligatorio",
+    '*': "revisa el campo 'img' ",
+  }),
+  price: joi.number().positive().required().messages({
+    'number.base': "El campo 'price' debe ser un número válido",
+    'number.positive': "El campo 'price' debe ser un número positivo",
+    'any.required': "El campo 'price' es obligatorio",
+    '*': "revisa el campo 'price' ",
   }),
   content: joi.string().trim().min(3).max(500).required().messages({
     'string.min': "El campo 'content' debe tener al menos 3 caracteres",
-    'string.max': "El campo 'content' debe tener menos de 500 caractere",
+    'string.max': "El campo 'content' debe tener menos de 500 caracteres",
     'any.required': "El campo 'content' es obligatorio",
     '*': "revisa el campo 'content' ",
+  }),
+  existens: joi.boolean().required().messages({
+    'any.required': "El campo 'existens' es obligatorio",
+    '*': "revisa el campo 'existens' ",
+  }),
+  veganas: joi.boolean().messages({
+    '*': "revisa el campo 'veganas' ",
+  }),
+  celiac: joi.boolean().messages({
+    '*': "revisa el campo 'celiac' ",
+  }),
+  vegetarian: joi.boolean().messages({
+    '*': "revisa el campo 'vegetarian' ",
+  }),
+  category: joi.string().trim().min(3).max(30).required().messages({
+    'string.min': "El campo 'category' debe tener al menos 3 caracteres",
+    'string.max': "El campo 'category' debe tener menos de 30 caracteres",
+    'any.required': "El campo 'category' es obligatorio",
+    '*': "revisa el campo 'category' ",
   }),
 });
--- src/models/productSchema.js
+++ src/models/productSchema.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
 const Product = new mongoose.Schema({
-  title: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  content: { type: String, required: true },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
+  nameP: { type: String, required: true }, // Nombre del producto
+  img: { type: String, required: true }, // URL de la imagen del producto
+  price: { type: Number, required: true }, // Precio del producto
+  content: { type: String, required: true }, // Descripción del producto
+  existens: { type: Boolean, default: true }, // Disponibilidad del producto
+  veganas: { type: Boolean, default: false }, // Opción vegana
+  celiac: { type: Boolean, default: false }, // Opción para celíacos
+  vegetarian: { type: Boolean, default: false }, // Opción vegetariana
+  category: { type: String, required: true }, // Categoría del producto
+  isActive: { type: Boolean, default: true }, // Indica si el producto está activo
 });
 export default mongoose.model('Product', Product);
